97dbefab88d78baa8ce53002342aa77b05edef8e
Luís Sousa (canuto491) <canuto491@gmail.com>
2019-08-25 12:59:46 +0100

    get one employee

--- src/routes/employees.js
+++ src/routes/employees.js
@@ -3,14 +3,30 @@ const router = express.Router();
 
 const mysqlConnection = require('../database');
 
-router.get('/', (req, res) => {
+router.get('/', (req, resp) => {
+    console.log('get all => employee');
+
     mysqlConnection.query('SELECT * FROM employees', (errors, rows, fields) => {
         if (errors) {
-            console.log(error)
+            console.log(error);
             return
         }
 
-        res.json(rows);
+        resp.json(rows);
+    });
+});
+
+router.get('/:id', (req, resp) => {
+    const { id } = req.params; // const id = req.params.id;
+    console.log('get one => employee with id', id);
+
+    mysqlConnection.query('SELECT * FROM employees WHERE id = ?', [id] , (errors, rows, fields) => {
+        if (errors) {
+            console.log(error);
+            return
+        }
+
+        resp.json(rows[0]);
     });
 });
 
