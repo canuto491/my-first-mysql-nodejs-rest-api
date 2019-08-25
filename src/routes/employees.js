798ac6c8e2d39614a8bbf8e335c04b67a0bb7e13
Luís Sousa (canuto491) <canuto491@gmail.com>
2019-08-25 11:31:31 +0100

    get all de los empleados, en la ruta inicial de la aplicación

--- src/routes/employees.js
+++ src/routes/employees.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+const mysqlConnection = require('../database');
 
+router.get('/', (req, res) => {
+    mysqlConnection.query('SELECT * FROM employees', (errors, rows, fields) => {
+        if (errors) {
+            console.log(error)
+            return
+        }
+
+        res.json(rows);
+    });
+});
 
 module.exports = router;
