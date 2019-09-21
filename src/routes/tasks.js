4285e49881925d5984becdb169724ea1ae69d7e1
Luís Sousa (canuto491) <canuto491@gmail.com>
2019-09-21 12:59:55 +0100

    inicia endpoints de ateras

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,4 +1,33 @@
 import { Router } from 'express';
 const router = Router();
 
-export default router;
+router.get('/', (req, resp) => {
+    console.log('get all => tasks');
+    resp.send('lista de tareas');
+});
+
+router.get('/:id', (req, resp) => {
+    const { id } = req.params;
+    console.log('get one => task with id', id);
+    resp.send('recibido');
+});
+
+router.post('/', (req, resp) => {
+    const id = null;
+    const { name, salary } = req.body;
+    resp.send('guardando tarea', req.body);
+});
+
+
+router.put('/:id', (req, resp) => {
+    const { id } = req.params;
+    resp.send('actualizando tarea con id', {id: id, data: req.body});
+
+});
+
+router.delete('/:id', (req, resp) => {
+    const { id } = req.params;
+    resp.send('borrando tarea con id', id);
+});
+
+export default router
