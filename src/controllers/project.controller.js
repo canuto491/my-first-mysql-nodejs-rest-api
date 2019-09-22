be2b08caa4b4608ded4ce101511576370e005544
Luís Sousa (canuto491) <canuto491@gmail.com>
2019-09-22 10:49:50 +0100

    crud de projects

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -1,11 +1,156 @@
 import type from "sequelize";
 import Project from '../models/Project';
 
-export async function createProject(request, response) {
-    console.log('request.body', request.body);
+export async function getProjects(request, response) {
+    try {
+        const projects = await Project.findAll({
+            logging: logQuery,
+            type: type.QueryTypes.SELECT,
+        });
+
+        return response.json({
+            data: projects,
+        });
+
+    } catch (error) {
+        console.error('Error on get all Projects:', error)
+
+        return response
+        .status(500)
+        .jason({
+            message: "Oops! something went wrong",
+            data: {},
+        });
+    }
+}
+
+export async function getProject(request, response) {
+    const { id } = request.params;
+
+    try {
+        const project = await Project.findOne({
+            where: {
+                id
+            },
+            logging: logQuery,
+            type: type.QueryTypes.SELECT,
+        });
+
+        return response.json(project);
+
+    } catch (error) {
+        console.error(`Error on get one Project [id=${id}]`, error)
+
+        return response
+        .status(500)
+        .json({
+            message: "Oops! something went wrong",
+            data: {},
+        });
+    }
+}
+
+export async function deleteProject(request, response) {
+    const { id } = request.params;
+
+    try {
+        const deleteRowCount = await Project.destroy({
+            where: {
+                id
+            },
+            logging: logQuery,
+            type: type.QueryTypes.SELECT,
+        });
+
+        if (deleteRowCount) {
+            return response.json({
+                message: "Project deleted succesfully",
+                count: deleteRowCount,
+            });
+        }
+
+        return response
+        .status(500)
+        .json({
+            message: "It was not possible to delete the resource",
+            count: deleteRowCount,
+        });
+
+    } catch (error) {
+        console.error(`Error on get one Project [id=${id}]`, error)
+
+        return response
+        .status(500)
+        .json({
+            message: "Oops! something went wrong",
+            data: {},
+        });
+    }
+}
+
+export async function updateProject(request, response) {
+    const { id } = request.params;
     const { name, priority, description, deliveryDate } = request.body;
 
-    let newProject = await Project.create(
+    const project = await Project.update(
+        {
+            name,
+            priority,
+            description,
+            deliveryDate,
+        },
+        {
+            where: { id },
+            logging: logQuery,
+            type: type.QueryTypes.UPDATE,
+        }
+    );
+
+    console.log(project);
+    if (project) {
+        return response.json({
+            message: 'Project updated Succesfully',
+            count: project,
+        })
+    }
+
+    return response
+    .status(500)
+    .json({
+        message: "Oops! something went wrong",
+        data: {},
+    });
+}
+
+
+export async function createProject(request, response) {
+    console.log('request.body', request.body);
+
+    try {
+        let newProject = await create(request.body);
+
+        if (newProject) {
+            return response.json({
+                message: 'Project Created Succesfully',
+                data: newProject
+            });
+        }
+    } catch (error) {
+        console.error('Error on Project Creation:', error);
+
+        return response
+        .status(500)
+        .json({
+            message: "Oops! something went wrong",
+            data: {},
+        });
+    }
+}
+
+async function create(RequestBody) {
+    const { name, priority, description, deliveryDate } = RequestBody;
+
+    const newProject = await Project.create(
         {
             name,
             priority,
@@ -13,6 +158,7 @@ export async function createProject(request, response) {
             deliveryDate
         },
         {
+            fields: ['name', 'priority', 'description', 'deliveryDate'],
             logging: logQuery,
             // plain: true,
             raw: false,
@@ -20,17 +166,8 @@ export async function createProject(request, response) {
         }
     );
 
-    if (newProject) {
-        response.json({
-            message: 'Project Created Succesfully',
-            data: newProject
-        });
-
-        return;
-    }
-    response.send('received');
+    return newProject;
 }
-
 
 function logQuery(query, modelData) {
     console.log('query',query);
--- src/routes/projects.js
+++ src/routes/projects.js
@@ -1,32 +1,18 @@
 import { Router } from 'express';
 const router = Router();
 
-import { createProject } from '../controllers/project.controller'
+import { createProject, getProjects, getProject, deleteProject, updateProject} from '../controllers/project.controller'
 
 // api/projects/
-router.get('/', (req, resp) => {
-    console.log('get all => tasks');
-    resp.send('lista de tareas');
-});
+router.get('/', getProjects);
 
-router.get('/:id', (req, resp) => {
-    const { id } = req.params;
-    console.log('get one => task with id', id);
-    resp.send('recebido');
-});
+router.get('/:id', getProject);
 
 router.post('/', createProject);
 
+router.delete('/:id', deleteProject);
 
-router.put('/:id', (req, resp) => {
-    const { id } = req.params;
-    resp.send('actualizando tarea con id', {id: id, data: req.body});
+router.put('/:id', updateProject);
 
-});
-
-router.delete('/:id', (req, resp) => {
-    const { id } = req.params;
-    resp.send('borrando tarea con id', id);
-});
 
 export default router
